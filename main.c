372a994a8b462b84ad6433a6bc659402f855d11c
Leandro Felix <leandro.almeida3029@gmail.com>
2022-07-14 15:27:44 -0300

    concertar func incicializarCampo

--- main.c
+++ main.c
@@ -9,17 +9,17 @@ typedef struct {
 }Celula;
 
 
-void inicializarCampo(int l, int c, int campo[10][20].eMina){
+void inicializarCampo(int l, int c, Celula campo[l][c]){
     for(int i=0; i<l; i++){
         for(int j=0; j<c; j++){
             campo[i][j].eMina = 0;
-            campo[i][j].vizinhos = 0;
+            campo[i][j].vizinhos= 0;
             campo[i][j].posVazia = 0;
         }
     }
 }
 
-void gerarMinas(){
+void gerarMinas(int l, int c, Celula campo[l][c], int minas){
     srand(time(NULL));
     int vetL;
     int vetC;
@@ -38,13 +38,13 @@ void preencherVizinhos(){
     
 }
 
-void exibirCampoAtual(){
+void exibirCampoAtual(int l, int c, Celula campo[l][c]){
     printf("    0  | 1  | 2  | 3  | 4  | 5  | 6  | 7  | 8  | 9  | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19\n");
     for(int i=0; i<l; i++){
         printf("  -----------------------------------------------------------------------------------------------------\n");
         printf("%d |", i);
         for(int j=0; j<c; j++){
-            printf("  %d|", campo[i][j].eMina);
+            printf("   |");
         }
         printf("\n");
     }
@@ -54,13 +54,13 @@ void exibirCampoAtual(){
 
 
 int main(){
-    Celula campo[10][20];
+    Celula campo[10];
     int l = 10, c = 20;
     int minas = 40;
 
-    inicializarCampo(l, c, campo[l][c].eMina);
-    gerarMinas();
-    exibirCampoAtual();
+    inicializarCampo(l, c, campo);
+    gerarMinas(l, c, campo, minas);
+    exibirCampoAtual(l, c, campo);
     
     return 0;
 }
