0f15c8c77d3344d339767da8f185b9922189d9e6
João Paulo <joaoroque317@gmail.com>
2022-07-21 10:36:22 -0300

    Organizando projeto
    Movi os arquivos para pasta principal para melhor visualização

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Jogo campo minado
 
-Projeto final da disciplina de introdução às técnicas de programação. <a href=IMD1012_-_Trabalho_3_-_Campo_Minado.pdf>Mais informação sobre o projeto</a>
+Projeto final da disciplina de introdução às técnicas de programação. <a href=/.instrucoes/IMD1012_-_Trabalho_3_-_Campo_Minado.pdf>Mais informação sobre o projeto</a>
 
--- main.c
+++ main.c
@@ -1,117 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-// Tamanho fixo de linha e coluna
-#define TamL 10
-#define TamC 20
-typedef struct {
-    int eMina;
-    int vizinhos;
-    int posAberta;
-}Celula;
 
+#include "constantes.h"
 
-void inicializarCampo(int l, int c, Celula campo[l][c]){
-    for(int i=0; i<l; i++){
-        for(int j=0; j<c; j++){
-            campo[i][j].eMina = 0;
-            campo[i][j].vizinhos= 0;
-            campo[i][j].posAberta = 0;
-        }
-    }
-}
+#include "funcoes.h"
 
-void gerarMinas(int l, int c, Celula campo[l][c], int minas){
-    srand(time(NULL));
-    int vetL;
-    int vetC;
-
-    for(int i=0; i<minas; i++){
-        vetL = rand() % 10; 
-        vetC = rand() % 20; 
-        if(campo[vetL][vetC].eMina == 0)
-            campo[vetL][vetC].eMina = 1;
-        else
-            i--;
-    }
-}
-// Verificar se a coordena é válida ou não
-int coordenaValida(int l, int c){
-    if(l >= 0 && l < TamL && c >= 0 && c < TamC){
-        return 1;
-    }
-    else{
-        return 0;
-    }
-}
-int quantMinasVizinhas(int l, int c, Celula campo[l][c]){
-    int quantMina = 0;
-    if(coordenaValida(l - 1, c) && campo[l-1][c].eMina){
-        quantMina ++;
-    }
-    if(coordenaValida(l + 1, c) && campo[l+1][c].eMina){
-        quantMina ++;
-    }
-    if(coordenaValida(l, c + 1) && campo[l][c+1].eMina){
-        quantMina ++;
-    }
-    if(coordenaValida(l, c - 1) && campo[l][c-1].eMina){
-        quantMina ++;
-    }
-    return quantMina;
-}
-
-
-
-// não sei se é a função correta
-void preencherVizinhos(Celula campo[TamL][TamC]){
-    for(int l = 0; l < TamL; l++){
-        for (int c = 0; c < TamC; c++)
-            campo[l][c].vizinhos = quantMinasVizinhas(l, c, campo);
-
-        
-    }
-}
-
-void exibirCampoAtual(int l, int c, Celula campo[l][c]){
-    printf("  |");
-    for(int i=0; i<c; i++)
-        if(i < 10)
-            printf(" %d  |", i);
-        else    
-            printf(" %d |", i);
-    printf("\n");
-    for(int i=0; i<l; i++){
-        printf("-------------------------------------------------------------------------------------------------------\n");
-        printf("%d |", i);
-        for(int j=0; j<c; j++){ 
-            if(campo[i][j].posAberta){
-                if(campo[i][j].eMina)
-                    printf(" *  |");
-                else   
-                    printf(" %d  |", campo[i][j].vizinhos); 
-
-            }
-            else{
-                printf("    |");
-            }
-        }
-        printf("\n");
-    }
-    printf("  -----------------------------------------------------------------------------------------------------\n");
-}
-
-
+#include "estruturas.h"
 
 int main(){
-    int l = 10, c = 20;
-    Celula campo[l][c];
+    Celula campo[10][20];
     
-    int minas = 40;
+    int modoJogo;
+    int opcao;
 
-    inicializarCampo(l, c, campo);
-    gerarMinas(l, c, campo, minas);
-    preencherVizinhos(campo);
-    exibirCampoAtual(l, c, campo);
+    printf("Escolha o modo de jogo:\n");
+    printf("1- Modo clássico\n");
+    printf("2- Modo autônomo\n");
+    printf("Modo: ");
+    scanf("%d", &modoJogo);
+    if(modoJogo == 1){
+        do{
+            inicializarCampo(campo);
+            gerarMinas(campo);
+            preencherVizinhos(campo);
+            jogar(campo);
+
+            printf("\nDigite 1 para jogar novamente: ");
+            printf("\nDigite 2 para pedir ajuda: \n");
+            scanf("%d", &opcao);
+        }while(opcao == 1);
+          
+    }
+           
+    
+    // gcc -o main main.c funcoes.c constantes.c
     return 0;
 }
